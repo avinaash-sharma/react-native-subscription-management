3b0e31f15f3e60c47821afb482738fc5d6b0d608
Avinash <avi.rohit13@hotmail.com>
2026-03-29 03:14:28 +0530

    typography completed

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,18 +8,16 @@ const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
     return (
         <SafeAreaView className="flex-1 bg-background p-5">
-            <Text className="text-xl font-bold text-blue-500">
-                Welcome to Nativewind!
+            
+        <Text className="text-5xl font-sans-extrabold">
+                Home
             </Text>
 
-            <Link href="/Onboarding" className="mt-4 rounded bg-primary text-white p-4">OnBoarding</Link>
-            <Link href="/sign-in" className="mt-4 rounded bg-primary text-white p-4">Sign In</Link>
-            <Link href="/sign-up" className="mt-4 rounded bg-primary text-white p-4">Sign Up</Link>
-            <Link href="/subscriptions/spotify" className="mt-4 rounded bg-primary text-white p-4">Spotify Subscriptions</Link>
-            <Link href={{
-                pathname: "/subscriptions/[id]",
-                params: { id: "Claude" },
-            }} className="mt-4 rounded bg-primary text-white p-4">Cloude Subscription</Link>
+            <Link href="/Onboarding" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">OnBoarding</Link>
+            <Link href="/sign-in" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">Sign In</Link>
+            <Link href="/sign-up" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">Sign Up</Link>
+            
+           
         </SafeAreaView>
     );
 }
